f4b259e44e6a3fee18e4951329de69fd81e3706b
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-28 15:53:51 +0000

    Add files via upload

--- main.tf
+++ main.tf
@@ -2,39 +2,26 @@ provider "aws" {
   region = "us-east-1"
 }
 
-  
 locals {
-  ssh_user         = "ubuntu"
-  key_name         = "test"
-  private_key_path = "test.pem"
+  ssh_user         = "ec2-user"
+  key_name         = "key"
+  private_key_path = "key.pem"
 }
 
-
 resource "aws_instance" "nginx" {
-  ami                         = "ami-0acd36292794047ab"
+  ami                         = "ami-0c2b8ca1dad447f8a"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   key_name                    = local.key_name
 
-  provisioner "remote-exec" {
-    inline = ["echo 'Wait until SSH is ready'"]
+  # provisioner "remote-exec" {
+  #   inline = ["echo 'Wait until SSH is ready'"]
 
-    connection {
-      type        = "ssh"
-      user        = local.ssh_user
-      private_key = file(local.private_key_path)
-      host        = aws_instance.nginx.public_ip
-    }
-  }
+ 
+  # }
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=True ansible-playbook  -i ${aws_instance.nginx.public_ip}, --private-key ${local.private_key_path} nginx.yaml"
+    command = "ansible-playbook  -i ${aws_instance.nginx.public_ip}, --private-key key.pem nginx.yaml"
   }
-}
-
- resource "time_sleep" "wait_30_seconds" {
-  depends_on = [aws_instance.nginx]
-
-  create_duration = "500s"
 }
 
 output "nginx_ip" {
